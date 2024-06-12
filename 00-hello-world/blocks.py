838c2d8055b48a04e57a849e81186c2600b3f4c6
Carlos Silva <carlosemilio.contato@gmail.com>
2024-06-12 11:54:44 -0300

    add: logic operators

--- 00-hello-world/blocks.py
+++ 00-hello-world/blocks.py
@@ -4,3 +4,14 @@ if idade < 18:
   print("Menor de idade")
 else: 
   print("maior de idade")
+
+# Operadores lógicos: AND - Executa se as duas condições forem verdadeiras 
+if True and True:
+  print("será executado")
+
+if True and False:
+  print("Não será executado")
+
+# Operadores lógicos: OR - Executa se pelo menos uma condição for verdadeira 
+if True or False:
+  print("será executado")
